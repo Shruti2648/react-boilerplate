64821595faa5f2004e207d8267509e7af05fe309
Shruti Jadhav <jadhavjshruti@gmail.com>
2019-07-11 20:21:17 -0400

    added react boilerplate files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+const Index = () => {
+  return <div id="home">
+      <p className="welcome">Welcome to React!</p>
+    </div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
